b3733b4388e14ba245dcae0a25fa0e6a3feddebc
Ziad.Abuolila <ziad.abuolila@gmail.com>
2025-04-07 18:53:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// loader
 window.onload = function () {
     setTimeout(function () {
         (function ($) {
@@ -9,30 +8,29 @@ window.onload = function () {
 }
 // menu
 document.addEventListener("DOMContentLoaded", () => {
-    const openMenuButton = document.getElementById("openMenu");
+    const openMenuButton = document.querySelector(".menu-btn");
     const menu = document.getElementById("menu");
-    const openMenu = () => {
-        menu.style.display = "block";
-    };
-    const closeMenu = () => {
-        menu.style.display = "none";
-    };
     const menuItems = menu.querySelectorAll("li a");
-    menuItems.forEach(item => {
-        item.addEventListener("click", closeMenu);
+    openMenuButton.addEventListener("click", () => {
+        menu.classList.toggle("d-none");
     });
-    openMenuButton.addEventListener("click", (e) => {
-        e.stopPropagation();
-        openMenu();
+    menuItems.forEach(item => {
+        item.addEventListener("click", () => {
+            menu.classList.add("d-none");
+        });
     });
     document.addEventListener("click", (e) => {
         if (!menu.contains(e.target) && e.target !== openMenuButton) {
-            closeMenu();
+            menu.classList.add("d-none");
         }
     });
     menu.addEventListener("click", (e) => {
         e.stopPropagation();
     });
+});
+document.getElementById("closeMenu").addEventListener("click", function () {
+    const menu = document.getElementById("menu");
+    menu.classList.add("d-none");
 });
 // Smooth Scroll
 const btn = document.getElementById("scrollTopBtn");
@@ -51,7 +49,6 @@ btn.addEventListener("click", () => {
 });
 // gallery
 const gallery = document.querySelector('.gallery');
-
 for (let i = 1; i <= 9; i++) {
     gallery.innerHTML += `
         <img src="img/photo/${i}.jpg" alt="Image ${i}">
@@ -59,7 +56,6 @@ for (let i = 1; i <= 9; i++) {
 }
 // confer
 const confer = document.querySelector('.confer');
-
 for (let i = 1; i <= 4; i++) {
     confer.innerHTML += `
         <img src="img/conference/${i}.jpg" alt="Image ${i}">
